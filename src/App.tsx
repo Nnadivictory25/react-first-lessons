0ac7227d3b3dc80cad5458102881c2d1226ec85f
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-17 02:19:16 +0100

    learnt alot about making http requests and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,88 @@
-import { useState } from 'react';
-import ProductList from './components/ProductList';
+import { useEffect, useState } from 'react';
+import userService, { User } from './services/user-service';
 
-function App() {
-  const [category, setCategory] = useState('');
 
-  const connect = () => console.log('connecting')
-  const disconnect = () => console.log('disconnecting')
 
-  return (
-    <div> 
-    </div>
-  );
+function App(): JSX.Element {
+	const [users, setUsers] = useState<User[]>([]);
+	const [error, setError] = useState('');
+	const [isLoading, setLoading] = useState(false);
+
+	useEffect(() => {
+
+		setLoading(true);
+    const { request, cancel} = userService.getAllUsers()
+			request.then((res) => {
+				setUsers(res.data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				if (err.message == 'canceled') {
+					return;
+				} else {
+					setError(err.message);
+					setLoading(false);
+				}
+			});
+
+		return () => cancel();
+	}, []);
+
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    
+    userService.deleteUser(user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      })
+  }
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: 'Kamas' }
+    setUsers([newUser, ...users]);
+
+    userService.createUser(newUser)
+      .then(({ data: SavedUser }) => setUsers([SavedUser, ...users]))
+      .catch(err => {
+        setError(err.message)
+        setUsers(originalUsers);
+      })
+  }
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + ' Kamas' }
+    setUsers(users.map((u) => u.id === user.id ? updatedUser : u))
+    
+    userService.updateUser(updatedUser)
+      .catch(err => {
+        setError(err.message)
+        setUsers(originalUsers)
+    })
+  }
+
+	return (
+		<>
+			{error && <p className='text-danger'>{error}</p>}
+      {isLoading && <div className='spinner-border'></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>Add User</button>
+			<ul className='list-group'>
+				{users.map((user) => (
+					<li key={user.id} className='list-group-item d-flex justify-content-between'>
+            {user.name} 
+            <div>
+            <button className='btn btn-outline-secondary mx-1' onClick={() => updateUser(user)}>Update</button>
+            <button className='btn btn-outline-danger' onClick={() => deleteUser(user)}>Delete</button>
+            </div>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
 
 export default App;
